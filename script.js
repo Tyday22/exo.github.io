89d5ff86ae00c6107e37709f1b0e1361c2115fe6
Ty Omonge <dayviedt@gmail.com>
2026-02-22 18:55:20 +0300

    Update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function page(pageid){
     window.location.href = pageid;
 }
+function download(file){
+    const link = document.createElement('a');
+    link.href = file;
+    link.download = '';
+    link.click();
+}
