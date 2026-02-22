990395ab2d9113e505b728a0ea810578d6f788f3
Ty Omonge <dayviedt@gmail.com>
2026-02-22 19:39:28 +0300

    Images

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let bgNum = 0;
+const container = document.getElementById('main');
+
 function page(pageid){
     window.location.href = pageid;
 }
@@ -7,3 +10,14 @@ function download(file){
     link.download = '';
     link.click();
 }
+function bg() {
+    bgNum++; 
+    if (bgNum > 7) bgNum = 1;    
+    container.style.backgroundImage = `url('../Images/BG${bgNum}.png')`;  
+    container.style.backgroundSize = "cover";
+    container.style.backgroundPosition = "center";
+    container.style.backgroundRepeat = "no-repeat";
+}
+
+bg(); 
+setInterval(bg, 10000);
